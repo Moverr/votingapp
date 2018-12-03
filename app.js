f4ede6354287e04c9ed80a954226dbf6eecc64cd
Muyinda Rogers <moverr@gmail.com>
2018-12-03 19:41:26 +0300

    Controllers Architecture setup :

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
+// var multer = require('multer');
+// var upload = multer();
 
 var app = express();
 
-app.use('/user','./routes/user.js');
-app.use('/profile','./routes/profile.js');
-app.use('/voting','./routes/voting.js');
-app.use('/candidate','./routes/candidate.js');
+app.use('/user',require('./controllers/user.js'));
+app.use('/profile',require('./controllers/profile.js'));
+app.use('/voting',require('./controllers/voting.js'));
+app.use('/candidate',require('./controllers/candidate.js'));
 
+
+const port = 8928;
+
+app.listen(port, () => {
+    console.log(`Server started on `+port);
+});
--- controllers/voting.js
+++ controllers/voting.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 router.get('/', (req, res) => {
-     res.json("Hello Rogers");
+     res.json("About to Vote");
 });
 
 module.exports = router;
