18809d7ce2a6c295077b473950365bc6ed137d4a
Muyinda Rogers <moverr@gmail.com>
2018-12-04 09:51:53 +0300

    :Debugging :

--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,4 @@
-var db = require("../db");
+// var db = require("../db");
 var util = require("../helpers/util");
 
 var utilHelper = new util();
@@ -8,7 +8,7 @@ class UserService {
     constructor() {}
 
     authenticate(username, password, callback) {
-        var connection = db.initConnection();
+        // var connection = db.initConnection();
 
         // connection.query('SELECT  * from user   WHERE user.username like "' + username + '"  AND  user.password LIKE "' + password + '"  limit 1 ', function (err, rows, fields) {
         //     if (err)
